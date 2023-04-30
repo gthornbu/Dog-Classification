ded58f3fc9f70a8ee427094cf9e3a926ce2c9b5a
gthornbu <gthornbu27@gmail.com>
2023-04-30 18:05:48 -0500

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,146 @@
+﻿namespace Dog_Classification
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnPredict = new System.Windows.Forms.Button();
+            this.btnUpload = new System.Windows.Forms.Button();
+            this.lblUpload = new System.Windows.Forms.Label();
+            this.lblPredict = new System.Windows.Forms.Label();
+            this.lblDescription = new System.Windows.Forms.Label();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.txtPath = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // btnPredict
+            // 
+            this.btnPredict.Location = new System.Drawing.Point(506, 152);
+            this.btnPredict.Name = "btnPredict";
+            this.btnPredict.Size = new System.Drawing.Size(75, 23);
+            this.btnPredict.TabIndex = 0;
+            this.btnPredict.Text = "Predict";
+            this.btnPredict.UseVisualStyleBackColor = true;
+            this.btnPredict.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // btnUpload
+            // 
+            this.btnUpload.Location = new System.Drawing.Point(200, 152);
+            this.btnUpload.Name = "btnUpload";
+            this.btnUpload.Size = new System.Drawing.Size(75, 23);
+            this.btnUpload.TabIndex = 1;
+            this.btnUpload.Text = "Upload";
+            this.btnUpload.UseVisualStyleBackColor = true;
+            this.btnUpload.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblUpload
+            // 
+            this.lblUpload.AutoSize = true;
+            this.lblUpload.Location = new System.Drawing.Point(156, 120);
+            this.lblUpload.Name = "lblUpload";
+            this.lblUpload.Size = new System.Drawing.Size(173, 13);
+            this.lblUpload.TabIndex = 2;
+            this.lblUpload.Text = "Upload an image of a dog to begin.";
+            // 
+            // lblPredict
+            // 
+            this.lblPredict.AutoSize = true;
+            this.lblPredict.Location = new System.Drawing.Point(432, 120);
+            this.lblPredict.Name = "lblPredict";
+            this.lblPredict.Size = new System.Drawing.Size(213, 13);
+            this.lblPredict.TabIndex = 3;
+            this.lblPredict.Text = "Click to predict the uploaded image\'s breed.";
+            // 
+            // lblDescription
+            // 
+            this.lblDescription.AutoSize = true;
+            this.lblDescription.Location = new System.Drawing.Point(208, 9);
+            this.lblDescription.Name = "lblDescription";
+            this.lblDescription.Size = new System.Drawing.Size(384, 13);
+            this.lblDescription.TabIndex = 4;
+            this.lblDescription.Text = "To use this application, please have python installed with tensorflow and numpy.";
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(272, 207);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(257, 165);
+            this.pictureBox1.TabIndex = 6;
+            this.pictureBox1.TabStop = false;
+            // 
+            // txtPath
+            // 
+            this.txtPath.Location = new System.Drawing.Point(231, 73);
+            this.txtPath.Name = "txtPath";
+            this.txtPath.Size = new System.Drawing.Size(327, 20);
+            this.txtPath.TabIndex = 7;
+            this.txtPath.Text = "C:\\ProgramData\\Anaconda3\\python.exe";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(289, 57);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(222, 13);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "Enter the file path for the python environment.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Inherit;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtPath);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.lblDescription);
+            this.Controls.Add(this.lblPredict);
+            this.Controls.Add(this.lblUpload);
+            this.Controls.Add(this.btnUpload);
+            this.Controls.Add(this.btnPredict);
+            this.Name = "Form1";
+            this.Text = "Dog Classification";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnPredict;
+        private System.Windows.Forms.Button btnUpload;
+        private System.Windows.Forms.Label lblUpload;
+        private System.Windows.Forms.Label lblPredict;
+        private System.Windows.Forms.Label lblDescription;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.TextBox txtPath;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Dog_Classification
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public void runPython()
+        {
+            string fileName = @"script.py";
+            string pythonPath = txtPath.Text;
+            if (pythonPath == "Graham's path")
+            {
+                pythonPath = @"C:\ProgramData\Anaconda3\envs\tensorflow_gpuenv\python.exe";
+            }
+            Process p = new Process();
+            p.StartInfo = new ProcessStartInfo(pythonPath, fileName)
+            {
+                RedirectStandardOutput = true,
+                UseShellExecute = false,
+                CreateNoWindow = true
+            };
+            p.Start();
+
+            string output = p.StandardOutput.ReadToEnd();
+            p.WaitForExit();
+            MessageBox.Show(output);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            runPython();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            // Create a new instance of the OpenFileDialog
+            OpenFileDialog openFileDialog1 = new OpenFileDialog();
+
+            // Set the filter for image files
+            openFileDialog1.Filter = "Image Files (*.jpg, *.jpeg, *.png, *.bmp)|*.jpg;*.jpeg;*.png;*.bmp";
+
+            // Set the initial directory to the user's Pictures folder
+            openFileDialog1.InitialDirectory = Environment.GetFolderPath(Environment.SpecialFolder.MyPictures);
+
+            // Display the OpenFileDialog by calling the ShowDialog method
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                // Get the file name and path of the selected image
+                string imagePath = openFileDialog1.FileName;
+
+                // Specify the path where you want to save the image
+                string savePath = @"image.jpg";
+
+                // Copy the selected image to the specified path
+                using (FileStream fileStream = new FileStream(savePath, FileMode.Create))
+                {
+                    using (BinaryWriter binaryWriter = new BinaryWriter(fileStream))
+                    {
+                        byte[] imageBytes = File.ReadAllBytes(imagePath);
+                        binaryWriter.Write(imageBytes);
+                    }
+                }
+                pictureBox1.SizeMode = PictureBoxSizeMode.StretchImage;
+                pictureBox1.Image = Image.FromFile(imagePath);
+
+            }
+        }
+    }
+}
